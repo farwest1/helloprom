b2b9880fb47eea88294a41165d4f0a0071e9aa48
Bernd <moeller-bernd@gmx.de>
2017-07-29 12:21:56 +0200

    Prometheus specific metrics

--- src/main/java/com/moeller/HelloPromController.java
+++ src/main/java/com/moeller/HelloPromController.java
@@ -1,5 +1,9 @@
 package com.moeller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,8 +15,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloPromController {
 
+  private final CounterService counterService;
+  private static final Logger LOGGER = LoggerFactory.getLogger(HelloPromController.class);
+
+  @Autowired
+  public HelloPromController(CounterService counterService){
+    this.counterService=counterService;
+  }
+
+
+
   @RequestMapping("/")
   public String index() {
+    LOGGER.debug("Basic Request called");
+    this.counterService.increment("services.api.index.invoked");
     return "Greetings from Spring Boot!";
   }
 }
